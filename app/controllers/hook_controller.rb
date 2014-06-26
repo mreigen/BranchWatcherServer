38315410a08d39c2b4a3a6e4775134cb6e6ae252
minh <mreigen@gmail.com>
2014-06-26 15:02:30 -0700

    send request to update to each subscriber

--- app/controllers/hook_controller.rb
+++ app/controllers/hook_controller.rb
@@ -1,13 +1,25 @@
 class HookController < ApplicationController
   skip_before_action :verify_authenticity_token
 
+  # TODO: move to controller concern
+  # or move to Subscriber model
+  def get_subscribers
+    subscriber_data = RunrWatcherServer.runr_session_get("subscribers")
+    return [] unless subscriber_data.present?
+    list = subscriber_data[:subscribers]["list"]
+    return [] unless list.present?
+    JSON.parse list
+  end
+
   def update
-    RunrWatcherServer.runr_send_request(
-      sent_to: "watcher1",
-      topic: "branch",
-      task: "update",
-      params: params
-    )
+    get_subscribers.each do |s|
+      RunrWatcherServer.runr_send_request(
+        sent_to: s,
+        topic: "branch",
+        task: "update",
+        params: params
+      )
+    end
     render json: {something: "here"}
   end
 end
